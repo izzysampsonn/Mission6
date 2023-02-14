6d24f90eb77d72552c2d7c9a95d3e0bddc66569c
Izzy Sampson <isabellesampson3@gmail.com>
2023-02-13 18:10:50 -0700

    Add project files.

---
+++ DateMeReal/Controllers/HomeController.cs
@@ -0,0 +1,64 @@
+﻿using DateMeReal.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DateMeReal.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private MovieEntryContext MovieContext { get; set; } // prep to pass data to database, set context file
+
+        // constructor
+        public HomeController(ILogger<HomeController> logger, MovieEntryContext someName)
+        {
+            _logger = logger;
+            MovieContext = someName;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Podcast()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Application()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Application(ApplicationEntry entry)
+        {
+            if (ModelState.IsValid)
+            {
+                MovieContext.Add(entry);
+                MovieContext.SaveChanges();
+                return View("confirmation", entry);
+            }
+            else
+            {
+                return View();
+            }
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ DateMeReal/Models/MovieEntryContext.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DateMeReal.Models
+{
+    public class MovieEntryContext : DbContext
+    {
+        public MovieEntryContext (DbContextOptions<MovieEntryContext> options): base (options) // constructor
+        {
+            // blank for now
+        }
+        public DbSet<ApplicationEntry> responses { get; set; } // set connection between database and the program
+
+        //seeding
+        protected override void OnModelCreating(ModelBuilder mb)
+        {
+            mb.Entity<ApplicationEntry>().HasData(
+                new ApplicationEntry
+                {
+                    EntryId = 1,
+                    MovieName = "La La Land",
+                    Category = "Romance",
+                    Year = "2016",
+                    Director = "Damien Chazelle",
+                    Rating = "PG-13",
+                    Edited = false,
+                    LentTo = "",
+                    Notes = "A really good movie that makes you think."
+                },
+                new ApplicationEntry
+                {
+                    EntryId = 2,
+                    MovieName = "Ratatouille",
+                    Category = "Action/Adventure",
+                    Year = "2007",
+                    Director = "Brad Bird",
+                    Rating = "PG",
+                    Edited = false,
+                    LentTo = "",
+                    Notes = "Vibes are unmatched."
+                },
+                new ApplicationEntry
+                {
+                    EntryId = 3,
+                    MovieName = "The Imitation Game",
+                    Category = "Action/Adventure",
+                    Year = "2014",
+                    Director = "Morten Tyldum",
+                    Rating = "PG-13",
+                    LentTo = "",
+                    Edited = false,
+                    Notes = ""
+                }
+            );
+        }
+    }
+}
